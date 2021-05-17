76f7f51d570f7523224426250ad2f98c0e0c52aa
MintoD <mintothedev@protonmail.com>
2021-05-17 13:11:11 +0700

    Update web

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,13 +77,13 @@ module.exports = {
                     sidebarPath: require.resolve("./sidebars.js"),
                     // Please change this to your repo.
                     editUrl:
-                        "https://github.com/B3taTeam/InnerCoreTutorials/",
+                        "https://github.com/B3taTeam/InnerCoreTutorials/edit/main/docs",
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     editUrl:
-                        "https://github.com/B3taTeam/InnerCoreTutorials",
+                        "https://github.com/B3taTeam/InnerCoreTutorials/edit/main/blog",
                 },
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
